e5944e235f2895681a9aa4348748f16c2014f1db
Michael Manhire <michaelmanhire@gmail.com>
2015-07-25 18:04:00 -0700

    Create HTML template

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Michael Manhire | Front-End Web Developer</title>
+<!--
+        <meta name="author" content="[author]">
+        <meta name="description" content="[description]">
+        <meta name="robots" content="[robots]">
+        <meta property="og:title" content="[title]">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="[image]">
+        <meta property="og:url" content="[url]">
+        <meta property="og:description" content="[description]">
+        <meta property="og:site_name" content="[site_name]">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="[site]">
+        <meta name="twitter:creator:id" content="[creator]">
+-->
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+        <!--[if lte IE 8]>
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+        <!--<![endif]-->
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/base-min.css">
+    </head>
+    <body>
+    
+    </body>
+</html>
