11053eacfa7196613a07176ec52f6f44e86049ae
Michael Manhire <michaelmanhire@gmail.com>
2015-07-31 08:14:33 -0700

    Adjust contact.php formatting

--- php/contact.php
+++ php/contact.php
@@ -2,7 +2,7 @@
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    $formContent = "From: $name \n Message: $message";
+    $formContent = "From: $name \r\n Message: $message";
     $recipient = "MichaelManhire@gmail.com";
     $subject = "Contact Form on MichaelManhire.com";
     $mailHeader = "From $email \r\n";
